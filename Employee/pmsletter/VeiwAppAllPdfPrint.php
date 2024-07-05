8390c1a4191e928dc7455f3a1bc824eb54155e34
Ajay <dewangan.ajay7@gmail.com>
2024-07-05 19:35:23 +0530

    Update VeiwAppAllPdfPrint.php

--- Employee/pmsletter/VeiwAppAllPdfPrint.php
+++ Employee/pmsletter/VeiwAppAllPdfPrint.php
@@ -463,7 +463,7 @@ $sqlP2=mysql_query("select DepartmentId from hrm_department where DepartmentName
 		  <!--<tr><td style="width:50px;">&nbsp;</td><td style="width:685px;font-size:18px;font-weight:bold;">&nbsp;</td><td style="width:50px;">&nbsp;</td></tr>-->
           <tr><td style="width:50px;">&nbsp;</td>
 		  <td style="width:685px;font-size:18px;"><?php if($_REQUEST['C']==1 AND ($_REQUEST['G']!='L3' AND $_REQUEST['G']!='L4' AND $_REQUEST['G']!='L5' AND $_REQUEST['G']!='MG' AND $_REQUEST['E']!=263)){echo '<img src="../images/lsign.jpg" border="0" />';}
-		  elseif($_REQUEST['C']==3 AND $_REQUEST['E']!=254){echo '<img src="../../AdminUser/images/devesh.png" border="0" width="150" />';}?></td>
+		  elseif($_REQUEST['C']==3 AND $_REQUEST['E']!=254){echo '<img src="../images/devesh.png" border="0" width="150" />';}?></td>
 		  <td style="width:50px;">&nbsp;</td></tr>
 		  <tr><td style="width:50px;">&nbsp;</td>
 		  <td>
