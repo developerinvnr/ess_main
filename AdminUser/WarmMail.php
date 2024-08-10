2c6c177bd9a5003e9991e86c2427aea24292a3b0
Ajay <dewangan.ajay7@gmail.com>
2024-08-10 10:33:44 +0530

    Remove warm mail extra file & update warm page

--- AdminUser/WarmMail.php
+++ AdminUser/WarmMail.php
@@ -5,30 +5,32 @@ require_once('config/config.php');
 mysql_query("SET SESSION sql_mode = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION'");
 date_default_timezone_set("Asia/Calcutta");
 
+if(date("Y-m-d")>=date("Y-m-15"))
+{
 
-if(date("m")>=2 && date("m")<=12){ $PrevMnt=date("m")-1; $PrevYer=date("Y"); }
+ if(date("m")>=2 && date("m")<=12){ $PrevMnt=date("m")-1; $PrevYer=date("Y"); }
       else{ $PrevMnt=12; $PrevYer=date("Y")-1;} 
       $mkdate = mktime(0,0,0, $PrevMnt, 1, $PrevYer); 
       $days = date('t',$mkdate);
       
-$WrmChk=mysql_query("select * from hrm_employee e inner join hrm_employee_general g ON e.EmployeeID=g.EmployeeID where g.DateJoining between '".date($PrevYer."-".$PrevMnt."-01")."' and '".date($PrevYer."-".$PrevMnt."-".$days)."' and e.EmpStatus='A' and e.CompanyId=1", $con); $RowWrmChk=mysql_num_rows($WrmChk); 
+ $WrmChk=mysql_query("select * from hrm_employee e inner join hrm_employee_general g ON e.EmployeeID=g.EmployeeID where g.DateJoining between '".date($PrevYer."-".$PrevMnt."-01")."' and '".date($PrevYer."-".$PrevMnt."-".$days)."' and e.EmpStatus='A' and e.CompanyId=1", $con); $RowWrmChk=mysql_num_rows($WrmChk); 
 
-if($RowWrmChk)
-{
+ if($RowWrmChk)
+ {
 
-$m=date("m"); 
-if($m==1){ $month='December'; $year=date("Y")-1; }
-else{ $mm=date("m")-1; $month=date("F",strtotime(date("Y-".$mm."-01"))); $year=date("Y"); }
+ $m=date("m"); 
+ if($m==1){ $month='December'; $year=date("Y")-1; }
+ else{ $mm=date("m")-1; $month=date("F",strtotime(date("Y-".$mm."-01"))); $year=date("Y"); }
 
-$email_message .= "<html><head><body>";
-$email_message .= "Dear VNRite,<p><br>\n\n";
-$email_message .= "We are pleased to welcome the new members of the VNR family.<br><br>\n";
-$email_message .= "Warm welcome details for the month of <b>".$month."-".$year."</b> is being displayed in ESS.<br><br>\n";
-$email_message .= "Login at ESS-->Click on Warm Welcome button on Main Page.<br><br>\n";
-$email_message .= "Please join us to welcome all new members to VNR Family.<p><p><p><br>\n\n\n";
-$email_message .= "Warm Regards<br>\n";
-$email_message .= "<b>Human Resources</b><br>\n\n";
-$email_message .= "</body></head></html>";
+ $email_message .= "<html><head><body>";
+ $email_message .= "Dear VNRite,<p><br>\n\n";
+ $email_message .= "We are pleased to welcome the new members of the VNR family.<br><br>\n";
+ $email_message .= "Warm welcome details for the month of <b>".$month."-".$year."</b> is being displayed in ESS.<br><br>\n";
+ $email_message .= "Login at ESS-->Click on Warm Welcome button on Main Page.<br><br>\n";
+ $email_message .= "Please join us to welcome all new members to VNR Family.<p><p><p><br>\n\n\n";
+ $email_message .= "Warm Regards<br>\n";
+ $email_message .= "<b>Human Resources</b><br>\n\n";
+ $email_message .= "</body></head></html>";
 
 
       $subject="Warm Welcome (".$month."-".$year.")";
@@ -69,6 +71,9 @@ while($res=mysql_fetch_assoc($sql))
 
 */
 
-} //if($RowWrmChk)
+ } //if($RowWrmChk)
+
+
+} //if(date("Y-m-d")>=date("Y-m-15"))
 
 ?>
